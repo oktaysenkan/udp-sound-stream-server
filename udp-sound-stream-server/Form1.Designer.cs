a080c93526f698169c9af1c5f0ce77c54aac168d
oktay.senkan <oktay.senkan@gmail.com>
2019-10-19 06:19:03 +0300

    Add project files.

---
+++ udp-sound-stream-server/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace udp_sound_stream_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblStatusValue = new System.Windows.Forms.Label();
+            this.lblIPAdress = new System.Windows.Forms.Label();
+            this.lblIPAdressValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 30);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(40, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "Status:";
+            // 
+            // lblStatusValue
+            // 
+            this.lblStatusValue.AutoSize = true;
+            this.lblStatusValue.Location = new System.Drawing.Point(74, 30);
+            this.lblStatusValue.Name = "lblStatusValue";
+            this.lblStatusValue.Size = new System.Drawing.Size(71, 13);
+            this.lblStatusValue.TabIndex = 1;
+            this.lblStatusValue.Text = "Waiting client";
+            // 
+            // lblIPAdress
+            // 
+            this.lblIPAdress.AutoSize = true;
+            this.lblIPAdress.Location = new System.Drawing.Point(12, 52);
+            this.lblIPAdress.Name = "lblIPAdress";
+            this.lblIPAdress.Size = new System.Drawing.Size(20, 13);
+            this.lblIPAdress.TabIndex = 2;
+            this.lblIPAdress.Text = "IP:";
+            // 
+            // lblIPAdressValue
+            // 
+            this.lblIPAdressValue.AutoSize = true;
+            this.lblIPAdressValue.Location = new System.Drawing.Point(74, 52);
+            this.lblIPAdressValue.Name = "lblIPAdressValue";
+            this.lblIPAdressValue.Size = new System.Drawing.Size(0, 13);
+            this.lblIPAdressValue.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 248);
+            this.Controls.Add(this.lblIPAdressValue);
+            this.Controls.Add(this.lblIPAdress);
+            this.Controls.Add(this.lblStatusValue);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblStatusValue;
+        private System.Windows.Forms.Label lblIPAdress;
+        private System.Windows.Forms.Label lblIPAdressValue;
+    }
+}
+
---
+++ udp-sound-stream-server/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSCore;
+using CSCore.Codecs.WAV;
+using CSCore.SoundIn;
+using CSCore.Streams;
+
+namespace udp_sound_stream_server
+{
+    public partial class Form1 : Form
+    {
+        UdpClient UDP;
+        IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 9050);
+        IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        private WasapiCapture soundIn;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            UDP = new UdpClient(serverEndPoint);
+            UDP.BeginReceive(DataReceived, null);
+        }
+
+        private void DataReceived(IAsyncResult ar)
+        {
+            byte[] data;
+            try
+            {
+                data = UDP.EndReceive(ar, ref clientEndPoint);
+
+                if (data.Length == 0)
+                    return; // No more to receive
+
+                //MessageBox.Show(Encoding.UTF8.GetString(data, 0, data.Length));
+            }
+            catch (ObjectDisposedException)
+            {
+                return; // Connection closed
+            }
+
+            // Send the data to the UI thread
+            this.BeginInvoke((Action<IPEndPoint, string>)DataReceivedUI, clientEndPoint, Encoding.UTF8.GetString(data));
+            StartAudioStreaming();
+        }
+
+        private void DataReceivedUI(IPEndPoint endPoint, string data)
+        {
+            lblStatusValue.Text = "Connected";
+            lblIPAdressValue.Text = endPoint.ToString();
+        }
+
+        private void StartAudioStreaming()
+        {
+            soundIn = new WasapiLoopbackCapture();
+            soundIn.Initialize();
+
+            SoundInSource soundInSource = new SoundInSource(soundIn) { FillWithZeros = false };
+            IWaveSource convertedSource = soundInSource
+                .ChangeSampleRate(44100) // sample rate
+                .ToSampleSource()
+                .ToWaveSource(16); //bits per sample
+            convertedSource = convertedSource.ToStereo();
+
+            WaveWriter waveWriter = new WaveWriter("dump.wav", convertedSource.WaveFormat);
+
+            soundInSource.DataAvailable += (s, e) =>
+            {
+                //read data from the converedSource
+                //important: don't use the e.Data here
+                //the e.Data contains the raw data provided by the 
+                //soundInSource which won't have your target format
+                byte[] buffer = new byte[convertedSource.WaveFormat.BytesPerSecond / 2];
+                int read;
+
+                //keep reading as long as we still get some data
+                //if you're using such a loop, make sure that soundInSource.FillWithZeros is set to false
+                while ((read = convertedSource.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    //write the read data to a file
+                    // ReSharper disable once AccessToDisposedClosure
+                    //waveWriter.Write(buffer, 0, read);
+                    UDP.Send(buffer, read, clientEndPoint);
+                }
+            };
+
+            soundIn.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (soundIn != null)
+                soundIn.Stop();
+        }
+    }
+}
