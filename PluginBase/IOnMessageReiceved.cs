dd87c813a5ddec69762f389fa8aa2e0defe0d5f5
Oktay Şenkan <oktay.senkan@gmail.com>
2019-12-31 16:05:47 +0300

    Plugin geliştirebilmek için temel oluşturuldu.

---
+++ PluginBase/IOnMessageReiceved.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginBase
+{
+    public interface IOnMessageReiceved
+    {
+        void OnMessageRecieved(IDictionary<string, string> Parameters);
+    }
+}
