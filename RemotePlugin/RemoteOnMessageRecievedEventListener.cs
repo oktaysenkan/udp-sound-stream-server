69de6ac9f7aa51a3802d517537d52e816102d520
Oktay Şenkan <oktay.senkan@gmail.com>
2019-12-31 16:06:22 +0300

    İlk test plugin'i geliştirildi.

---
+++ RemotePlugin/RemoteOnMessageRecievedEventListener.cs
@@ -0,0 +1,18 @@
+﻿using PluginBase;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemotePlugin
+{
+    public class RemoteOnMessageRecievedEventListener : IOnMessageReiceved  
+    {
+        public void OnMessageRecieved(IDictionary<string, string> parameters)
+        {
+            Debug.WriteLine("Test");
+        }
+    }
+}
